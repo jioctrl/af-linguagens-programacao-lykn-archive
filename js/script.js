ee36c6d93a57fead297f9049829569707a4aae2e
jioctrl <gfzorzenone@gmail.com>
2026-06-05 00:41:04 -0300

    Começo do index + contato

--- js/script.js
+++ js/script.js
@@ -5,3 +5,61 @@ Aluno: Giovana Fernandes Zorzenone - RA: 251804
 Professor: Abimael de Oliveira
 Data de entrega: 07/06
 */
+
+// Formulario
+const formulario = document.querySelector('#formContato');
+
+if (formulario) {
+    formulario.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const nome = document.querySelector('#nome').value;
+        const email = document.querySelector('#email').value;
+        const mensagem = document.querySelector('#mensagem').value;
+
+        if (nome === '' || email === '' || mensagem === '') {
+            alert('Por favor, preencha os campos obrigatórios.');
+        } else {
+            alert('Mensagem enviada com sucesso!');
+            formulario.reset();
+        }
+    });
+}
+
+// Carrossel membros
+const slides = document.querySelectorAll(".slide, .slide-ativo");
+const btnAnterior = document.querySelector(".anterior");
+const btnProximo = document.querySelector(".proximo");
+
+let slideAtual = 0;
+
+function mostrarSlide(indice) {
+
+    slides.forEach(slide => {
+        slide.classList.remove("slide-ativo");
+        slide.classList.add("slide");
+    });
+
+    slides[indice].classList.remove("slide");
+    slides[indice].classList.add("slide-ativo");
+}
+
+btnProximo.addEventListener("click", () => {
+    slideAtual++;
+
+    if (slideAtual >= slides.length) {
+        slideAtual = 0;
+    }
+
+    mostrarSlide(slideAtual);
+});
+
+btnAnterior.addEventListener("click", () => {
+    slideAtual--;
+
+    if (slideAtual < 0) {
+        slideAtual = slides.length - 1;
+    }
+
+    mostrarSlide(slideAtual);
+});
